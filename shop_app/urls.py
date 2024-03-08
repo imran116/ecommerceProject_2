f821d2c31bf0fcbd023afc160ecb961e7aba6a12
imran116 <ih9114101@gmail.com>
2024-03-08 21:18:50 +0600

    ADD: product_details.html in shop_app , include add to cart option and product details

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -5,5 +5,6 @@ app_name = 'shop_app'
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('product/<pk>', views.ProductDetail.as_view(), name='product-detail'),
 
 ]
--- shop_app/views.py
+++ shop_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
-# Improt Views
+# Import Views
 from django.views.generic import ListView, DeleteView
 
 # Models
 from shop_app.models import Product
+# Mixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
@@ -12,3 +14,8 @@ from shop_app.models import Product
 class Home(ListView):
     model = Product
     template_name = 'shop_app/home.html'
+
+
+class ProductDetail(LoginRequiredMixin, DeleteView):
+    model = Product
+    template_name = 'shop_app/product_detail.html'
